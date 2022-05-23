ae7cc31d765cb65296125e37b8f8276c7976d25b
Niall Finlay <niall.finlay@gmail.com>
2022-05-23 12:05:08 +0100

    module1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def load_module(directory, name):
   sys.path.insert(0, directory)
-  importlib.import_module(name=name)
+  importlib.import_module(name)
   sys.path.pop(0)
 
 def load_directory(directory):
